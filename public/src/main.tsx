a6928f2ba4830cf4896584822756e037ade05861
CBRT513 <clif@barge2rail.com>
2025-08-19 15:14:20 -0400

    Update main.tsx

--- public/src/main.tsx
+++ public/src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
+const rootEl = document.getElementById('root');
+if (!rootEl) throw new Error("Root element #root not found");
+
+createRoot(rootEl).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
